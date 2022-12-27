219e29ff5b266d12cbb387394b8bedb94c59a13e
Si9num <yastremskykirill@gmail.com>
2022-12-27 20:18:24 +0300

    add styles

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./index.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+    assetModuleFilename: "assets/[name][ext]",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "King Crimson",
+      template: path.resolve(__dirname, "./index.html"),
+      filename: "index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./img"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./index.css",
+    }),
+  ],
+  mode: "development",
+};
